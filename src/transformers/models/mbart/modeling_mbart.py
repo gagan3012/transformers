2a292cbbf0c2891b5f626e9dcaadc4fe567bce28
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2022-11-15 21:52:27 -0800

    Update modeling_mbart.py

--- src/transformers/models/mbart/modeling_mbart.py
+++ src/transformers/models/mbart/modeling_mbart.py
@@ -96,6 +96,7 @@ def _make_causal_mask(input_ids_shape: torch.Size, dtype: torch.dtype, past_key_
     """
     Make causal mask used for bi-directional self-attention.
     """
+    print(input_ids_shape)
     bsz, tgt_len = input_ids_shape
     mask = torch.full((tgt_len, tgt_len), torch.tensor(torch.finfo(dtype).min))
     mask_cond = torch.arange(mask.size(-1))
